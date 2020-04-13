d5023b0ea408e4c1be2ac967d1e03451f2f555f5
unknown <Srii1020>
2020-04-13 22:53:03 +0530

    added constructor

---
+++ LRU-oops/Lru_cache.py
@@ -0,0 +1,8 @@
+class LRUCache:
+    def __init__(self,capacity :int):
+        self.capacity = capacity
+    def put(self,key:int,value:int):
+        pass
+    def get(self,key:int): 
+        pass
+    def get_cache(self):            
